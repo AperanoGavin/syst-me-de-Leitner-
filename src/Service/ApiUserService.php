0bf3c4691a9cf6e8993f77543c7de607a5700538
Gavin <90148140+AperanoGavin@users.noreply.github.com>
2023-12-28 17:06:54 +0100

    making test not finish

--- src/Service/ApiUserService.php
+++ src/Service/ApiUserService.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace App\Service;
+use Doctrine\ORM\Query\Expr\Base;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 
-Class ApiUserService{
+Class ApiUserService {
 
     private $uri = 'http://localhost:8000/api/users?';
    public function __construct( private HttpClientInterface $client, string $uri) {
--- tests/Service/ApiUserServiceTest.php
+++ tests/Service/ApiUserServiceTest.php
@@ -2,16 +2,32 @@
 
 namespace App\Tests\Service;
 
+use app\Service\ApiUserService;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class ApiUserServiceTest extends KernelTestCase
 {
-    public function testSomething(): void
+    public function testGetUserByEmail()
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+
+        $apiService = $container->get(ApiUserService::class);
+        $api = $apiService->getUser('aude');
+
+        $result = trim(ob_get_clean());
+
+        //ce qu'il y'a dans la variable $result est égal à ce qu'il y'a dans la variable $api
+        $this->assertEquals($api, $result);
+        
+        return $result;
+    }
+   /*   public function testSomething(): void
     {
         $kernel = self::bootKernel();
 
         $this->assertSame('test', $kernel->getEnvironment());
         // $routerService = static::getContainer()->get('router');
         // $myCustomService = static::getContainer()->get(CustomService::class);
-    }
+    }  */
 }
