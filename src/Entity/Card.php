856ec1ee38008ba525db401fd1518d85be267398
Gavin APERANO MOULOUNGUI <gavin.aperano-mouloungui@ekino.com>
2024-02-29 13:18:44 +0100

    versionnig ok

--- src/Entity/Card.php
+++ src/Entity/Card.php
@@ -3,37 +3,45 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
+use Symfony\Component\Serializer\Annotation\Groups;
 use App\Repository\CardRepository;
 use Doctrine\ORM\Mapping as ORM;
 use App\Entity\CategoryInterface;
 
-#[ORM\Entity(repositoryClass: CardRepository::class)]
-#[ApiResource]
-#[ApiResource(
-    normalizationContext: ["exclude" => ["category"]],
-    denormalizationContext: ["exclude" => ["category"]]
-)]
 
+#[ORM\Entity(repositoryClass: CardRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(),
+    ])
+]
 
 class Card
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-
-
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['read'])]
     private string $category;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['read', 'write'])]
     private string $question;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['read', 'write'])]
     private string $answer;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['read', 'write'])]
     private string $tag;
 
     public function __construct()
@@ -46,11 +54,13 @@ class Card
         return $this->id;
     }
 
+    #[Groups(['read'])]
     public function getCategory(): string
     {
         return $this->category;
     }
 
+    #[Groups(['write'])]
     public function setCategory(string $category): self
     {
         $this->category = $category;
@@ -58,11 +68,13 @@ class Card
         return $this;
     }
 
+    #[Groups(['read'])]
     public function getQuestion(): string
     {
         return $this->question;
     }
 
+    #[Groups(['write'])]
     public function setQuestion(string $question): self
     {
         $this->question = $question;
@@ -70,11 +82,13 @@ class Card
         return $this;
     }
 
+    #[Groups(['read'])]
     public function getAnswer(): string
     {
         return $this->answer;
     }
 
+    #[Groups(['write'])]
     public function setAnswer(string $answer): self
     {
         $this->answer = $answer;
@@ -82,11 +96,13 @@ class Card
         return $this;
     }
 
+    #[Groups(['read'])]
     public function getTag(): string
     {
         return $this->tag;
     }
 
+    #[Groups(['write'])]
     public function setTag(string $tag): self
     {
         $this->tag = $tag;
